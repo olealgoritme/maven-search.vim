2b3fd6b05bccbcd2d47bfc6527ae3154e06b52f9
Ole Algoritme <olealgoritme@gmail.com>
2019-10-21 13:38:00 +0200

    changed to python3 semantics

--- search_repo.py
+++ search_repo.py
@@ -1,20 +1,29 @@
 #!/usr/bin/python
 import sys
-from urllib import urlopen
+from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
 # current https://mvnrepository.com/search?q=queryString
 # <h2 class="im-title"><span style="font-weight: normal; font-size:90%; color:gray">1. </span><a href="/artifact/org.flywaydb/flyway-core">Flyway Core</a><a class="im-usage" href="/artifact/org.flywaydb/flyway-core/usages"><b>414</b> usages</a></h2>
+_REPO = 'https://mvnrepository.com/'
 
-def get_url(url):
+def search_repo(url):
     page = urlopen(url)
-    soup = BeautifulSoup(page, 'html.parser')
-    res_list = soup.find_all('h2', class_="im-title")
+    _soup = BeautifulSoup(page, 'html.parser')
+    res_list = _soup.find_all('h2', class_="im-title")
 
     new_list = '';
     for item in res_list:
-        fixed_item = str(item.span.text + " " + item.a.text + " (" + item.a['href'].replace("/artifact/", "").replace("/", ".") + ")").encode('utf-8')
-        new_list += fixed_item + str('\n').encode('utf-8')
+        new_list += item.span.text
+        new_list += " "
+        new_list += item.a.text
+        new_list += " ("
+        new_list += item.a['href'].replace("/artifact/", "").replace("/", ".")
+        new_list += ")"
+        new_list += '\n'
+
     return new_list[:-1]
 
-print get_url("https://mvnrepository.com/search?q=" + sys.argv[1])
+
+url = _REPO + "search?q=" + sys.argv[1]
+print(search_repo(url))
